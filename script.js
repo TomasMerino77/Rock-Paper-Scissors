4eb347dbf1c0b59c07225405c1ec2c076a41825f
Tomas Merino <tomasmerinomm@gmail.com>
2025-05-31 00:59:48 +0000

    Adds default case to avoid incorrect values

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 //Ask user to imput a move
 let userChoice = prompt("Enter a value between 0 and 2.\n0 = Rock. 1 = Paper. 2 = Scissors");
 userChoice = parsePlay(+userChoice);
-console.log(userChoice);
 
 let computerChoice = Math.floor(Math.random() * 3);
 computerChoice = parsePlay(computerChoice);
-console.log(computerChoice);
+
 
 
 function parsePlay(choice){
@@ -16,5 +15,6 @@ function parsePlay(choice){
                     return "Paper";
                 case 2:
                     return "Scissors";
+                default: console.log("Incorrect value");
             }
 }
