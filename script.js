5b5fc09ce3d2880f780dac67446568ea92d6380f
Tomas Merino <tomasmerinomm@gmail.com>
2025-05-31 01:12:43 +0000

    Completes the logic for game evaluation

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-//Ask user to imput a move
 let userChoice = prompt("Enter a value between 0 and 2.\n0 = Rock. 1 = Paper. 2 = Scissors");
 userChoice = parsePlay(+userChoice);
+console.log(`User choice: ${userChoice}`);
 
 let computerChoice = Math.floor(Math.random() * 3);
 computerChoice = parsePlay(computerChoice);
+console.log(`Computer choice: ${computerChoice}`);
 
-
+if (
+    computerChoice === "Rock" && userChoice === "Scissors" ||
+    computerChoice === "Paper" && userChoice === "Rock" ||
+    computerChoice === "Scissors" && userChoice === "Paper"
+) {
+    console.log("Computer wins");
+}
+else if (computerChoice === userChoice) {
+    console.log("Tie");
+}
+else {
+    console.log("User Wins");
+}
 
 function parsePlay(choice){
     switch(choice){
