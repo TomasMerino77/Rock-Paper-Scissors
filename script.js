70a52dea956a8b60d7d42688c813dc35c9ac67b2
Tomas Merino <tomasmerinomm@gmail.com>
2025-05-31 01:25:15 +0000

    Adds scoreboard

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-let userChoice = prompt("Enter a value between 0 and 2.\n0 = Rock. 1 = Paper. 2 = Scissors");
-userChoice = parsePlay(+userChoice);
-console.log(`User choice: ${userChoice}`);
 
-let computerChoice = Math.floor(Math.random() * 3);
-computerChoice = parsePlay(computerChoice);
-console.log(`Computer choice: ${computerChoice}`);
+let userScore = 0;
+let computerScore = 0;
+let tieScore = 0;
 
-if (
-    computerChoice === "Rock" && userChoice === "Scissors" ||
-    computerChoice === "Paper" && userChoice === "Rock" ||
-    computerChoice === "Scissors" && userChoice === "Paper"
-) {
-    console.log("Computer wins");
-}
-else if (computerChoice === userChoice) {
-    console.log("Tie");
-}
-else {
-    console.log("User Wins");
-}
+function playRound(){
+    let userChoice = prompt("Enter a value between 0 and 2.\n0 = Rock. 1 = Paper. 2 = Scissors");
+    userChoice = parsePlay(+userChoice);
+    console.log(`User choice: ${userChoice}`);
+    
+    let computerChoice = Math.floor(Math.random() * 3);
+    computerChoice = parsePlay(computerChoice);
+    console.log(`Computer choice: ${computerChoice}`);
+    
+    if (
+        computerChoice === "Rock" && userChoice === "Scissors" ||
+        computerChoice === "Paper" && userChoice === "Rock" ||
+        computerChoice === "Scissors" && userChoice === "Paper"
+    ) {
+        computerScore++;
+        console.log("Computer wins");
+    }
+    else if (computerChoice === userChoice) {
+        tieScore++;
+        console.log("Tie");
+    }
+    else {
+        userScore++;
+        console.log("User Wins");
+    }
 
+    console.log(`Total scores:
+        User: ${userScore}.
+        Computer: ${computerScore}.
+        Ties: ${tieScore}.`)
+}
+    
 function parsePlay(choice){
     switch(choice){
                 case 0:
